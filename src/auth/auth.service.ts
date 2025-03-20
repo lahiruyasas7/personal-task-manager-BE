c6ac77f0b2c113b5a6ec6c86fe33c6d7acd62370
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-03-20 16:37:35 +0530

    created jwt auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {}
+
+@Injectable()
+export class JWTAuthService {
+  constructor(
+    private configService: ConfigService,
+    private readonly jwtServ: JwtService,
+  ) {}
+
+  validateToken(token: string) {
+    return this.jwtServ.verify(token, {
+      secret: process.env.JWT_SECRET,
+    });
+  }
+}
