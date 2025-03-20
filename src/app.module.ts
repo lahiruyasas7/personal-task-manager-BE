c974e33a048cd2de4090c13575784b659289bee3
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-03-20 15:44:54 +0530

    added taskModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/personal_task_manager_db')],
+  imports: [MongooseModule.forRoot('mongodb://localhost/personal_task_manager_db'), TasksModule],
   controllers: [AppController],
   providers: [AppService],
 })
