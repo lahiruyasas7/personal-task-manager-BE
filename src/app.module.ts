605c7aaa94e15a9c7760bd4e8c64218a36a3759e
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-03-21 11:00:51 +0530

    added configService global

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,16 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/personal_task_manager_db'),
     TasksModule,
     AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true, // Ensures ConfigService is available globally
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
