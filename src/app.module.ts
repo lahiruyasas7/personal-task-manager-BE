cb3e74d40c3cb6cdc14626cc3454aa6586c09b67
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-03-21 22:07:52 +0530

    created folder structure for category

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { CategoryModule } from './category/category.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { ConfigModule } from '@nestjs/config';
     ConfigModule.forRoot({
       isGlobal: true, // Ensures ConfigService is available globally
     }),
+    CategoryModule,
   ],
   controllers: [AppController],
   providers: [AppService],
