ea2a8ff10f6692c87304100c96dec08251b1a424
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-03-21 17:52:32 +0530

    added user module

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -3,6 +3,7 @@ import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Task, TaskSchema } from 'src/schemas/tasks.scehma';
+import { User, UserSchema } from 'src/schemas/user.schema';
 
 @Module({
   imports: [
@@ -10,6 +11,10 @@ import { Task, TaskSchema } from 'src/schemas/tasks.scehma';
       {
         name: Task.name,
         schema: TaskSchema,
+      },
+      {
+        name: User.name,
+        schema: UserSchema,
       },
     ]),
   ],
