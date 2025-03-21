6eb16a2cad936e3584319fff2d6a2673e04d46d2
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-03-21 11:01:33 +0530

    created register user endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { RegisterUserDto } from './dto/registerUser.dtp';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('/create')
+  createTask(@Body() registerUserDto: RegisterUserDto) {
+    return this.authService.registerUser(registerUserDto);
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,53 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  Logger,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from 'src/schemas/user.schema';
+import { RegisterUserDto } from './dto/registerUser.dtp';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  private logger: Logger = new Logger(AuthService.name);
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  async registerUser(registerUserDto: RegisterUserDto) {
+    try {
+      const { userName, email, password } = registerUserDto;
+
+      // hash password using bcrypt
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      // Check existing user
+      const existingUser = await this.userModel.findOne({ email });
+      if (existingUser) {
+        throw new ConflictException('Email is already in use');
+      }
+      const newUser = new this.userModel({
+        userName,
+        email,
+        password: hashedPassword,
+      });
+
+      return await newUser.save();
+    } catch (error) {
+      this.logger.error(`register ${error}`);
+
+      // conflict errors
+      if (error instanceof ConflictException) {
+        throw error;
+      }
+      // catch errors
+      throw new UnauthorizedException('Failed to register new user');
+    }
+  }
+}
 
 @Injectable()
 export class JWTAuthService {
