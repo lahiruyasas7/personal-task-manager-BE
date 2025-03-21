65eba3d73e17e28b636fafe05187c5c6695d98d0
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-03-21 22:33:26 +0530

    created create category endpoint

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { CategoryService } from './category.service';
+import { CreateCategoryDto } from './dto/create-category.dto';
 
 @Controller('category')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
+
+  @Post('/create')
+  createTask(@Body() createCategoryDto: CreateCategoryDto) {
+    return this.categoryService.createCategory(createCategoryDto);
+  }
 }
--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CategoryController } from './category.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Category, CategorySchema } from 'src/schemas/category.schema';
+import { User, UserSchema } from 'src/schemas/user.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Category.name,
+        schema: CategorySchema,
+      },
+      {
+        name: User.name,
+        schema: UserSchema,
+      },
+    ]),
+  ],
   controllers: [CategoryController],
   providers: [CategoryService],
 })
