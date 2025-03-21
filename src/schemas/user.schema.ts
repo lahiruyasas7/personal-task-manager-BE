c02becb738e021c15de0de1a3e8f13e7e5651480
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-03-21 17:57:36 +0530

    set relationship between user and tasks

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -1,4 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Types } from 'mongoose';
+import { Task } from './tasks.scehma';
 
 @Schema({ timestamps: true })
 export class User {
@@ -10,6 +12,9 @@ export class User {
 
   @Prop({ required: true })
   password: string;
+
+  @Prop({ type: [{ type: Types.ObjectId, ref: 'Task' }] })
+  tasks?: Task[];
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
