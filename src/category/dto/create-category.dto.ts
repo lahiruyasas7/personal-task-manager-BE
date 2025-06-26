c888c8d7e736d6cf58ae9e2ce1e7abbf41a9725e
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-06-26 22:10:15 +0530

    added swagger decorators

--- src/category/dto/create-category.dto.ts
+++ src/category/dto/create-category.dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateCategoryDto {
+  @ApiProperty({ description: 'The Name of the category', example: 'Food' })
   @IsNotEmpty()
   @IsString()
   categoryName: string;
 
+  @ApiProperty({ description: 'The user id', example: '' })
   @IsString()
   @IsNotEmpty()
   user: string;
